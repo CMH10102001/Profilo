5d53ddb5a307f0b61c0b9bef509d70ac3c176242
Tommy <tommyhk1010@gmail.com>
2023-12-04 00:01:52 +0800

    New

--- Series/src/App.js
+++ Series/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        
-      </header>
-    </div>
+    <>
+    <></>
+    <nav className="Bar">
+    Home   Place  Genre  
+    <h1 className='description'>Latest Show</h1>
+    </nav>
+    <p>This is the website for who want to see latest Series and </p>
+    </>
   );
 }
 
